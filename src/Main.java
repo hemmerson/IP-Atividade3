38ed782e1566347ef1b522bb7a57f156b577d9b2
Hemmerson Luis Barros da Rosa <hemmerson.rosa@gmail.com>
2023-02-23 19:08:45 -0300

    commit com o programa rodando!

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 /**
- * @author ${USER}
- * on date ${DATE}
+ * @author hemmerson
+ * on date 23/02/2023
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int numero;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String parImpar = "";
+            System.out.println("Digite um numero para saber se é par ou impar: ");
+            numero = scanner.nextInt();
+
+            if (numero % 2 == 0)
+                parImpar = "PAR";
+            else
+                parImpar = "IMPAR";
+
+            System.out.printf("O número digitado: %d, é %s\n", numero, parImpar);
+        }
     }
 }
